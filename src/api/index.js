3755755ce0a4bb0c633d00b90558ad54c4a83398
LightPride <vlad.grinevich1@gmail.com>
2023-06-23 17:22:20 +0300

    project setup

---
+++ src/api/index.js
@@ -0,0 +1,13 @@
+// const BASE_URL = 'https://pixabay.com/api/';
+
+// const API_KEY = '35698435-4c63849f1d133deb699669e72';
+
+// export const getImages = async ({ nextQuery, nextPage }) => {
+//   const response = await fetch(
+//     `${BASE_URL}?q=${nextQuery}&page=${nextPage}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+//   );
+//   if (!response.ok) {
+//     throw new Error('Oops... something went wrong...');
+//   }
+//   return response.json();
+// };
